4c749f13bdbfb21306b59589ab09161bf25a56b3
幸成 <573105494@qq.com>
2020-02-28 15:08:37 +0800

    先去回顾一下vue的基础知识

---
+++ client/src/views/404.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="notfound">
+        <img src="../assets/404.gif" alt="找不到页面">
+    </div>
+</template>
+
+<style scoped>
+.notfound{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+
+.notfound img{
+    position: absolute;
+    left: 27%;
+    height: 100%;
+}
+</style>
---
+++ client/src/views/Register.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="register">
+    <section class="form_container">
+      <div class="manage_tip">
+        <span class="title">Arthur在线后台管理系统</span>
+        <el-form
+          :model="registerUser"
+          :rules="rules"
+          ref="registerForm"
+          label-width="80px"
+          class="registerForm"
+        >
+          <el-form-item label="用户名" prop="name">
+            <el-input v-model="registerUser.name" placeholder="请输入用户名"></el-input>
+          </el-form-item>   
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="registerUser.name" placeholder="请输入用户名"></el-input>
+          </el-form-item>   
+          <el-form-item label="用户名" prop="name">
+            <el-input v-model="registerUser.name" placeholder="请输入用户名"></el-input>
+          </el-form-item>   
+          <el-form-item>
+            <el-button @click="resetForm('ruleForm')">注册</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "register",
+  components: {}
+};
+</script>
+
+<style scoped>
+.register {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: url(../assets/bg.jpg) no-repeat center center;
+  background-size: 100% 100%;
+}
+.form_container {
+  width: 370px;
+  height: 210px;
+  position: absolute;
+  top: 10%;
+  left: 34%;
+  padding: 25px;
+  border-radius: 5px;
+  text-align: center;
+}
+.form_container .manage_tip .title {
+  font-family: "Microsoft YaHei";
+  font-weight: bold;
+  font-size: 26px;
+  color: #fff;
+}
+</style>
